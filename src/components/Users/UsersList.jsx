af1fe7d8f2440677e60635a9c5d8fe187674a13c
Abhilash Nair <digilabcloudhub@gmail.com>
2023-03-13 15:10:02 +0000

    second

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -10,7 +10,7 @@ const UsersList = () => {
    const [active, setIsActive] = useState(null);
 
       const selectUser =(e)=>{
-        console.log(active);
+        
         fetchData();
        
       }
